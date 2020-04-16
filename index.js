190889400f9a1b0d23d725f6f0d4b0bfde98e26d
agboola.omowumi <agboola.omowumi@konga.com>
2020-04-16 22:19:52 +0100

    Diff Two ArraysPassed

--- index.js
+++ index.js
@@ -26,3 +26,27 @@ function sumAll(arr) {
 }
 
 sumAll([5, 10]);
+
+// Intermediate Algorithm Scripting: Diff Two ArraysPassed
+// Compare two arrays and return a new array with any items only found in one of the two given arrays, but not both. In other words, return the symmetric difference of the two arrays.
+
+// Note
+// You can return the array with its elements in any order.
+
+function diffArray(arr1, arr2) {
+  const compareFunc = (a, b) => {
+    return a - b;
+  };
+  var newArr = [];
+  let mergeArr = [...arr1, ...arr2];
+  const filterFunc = num => {
+    let res1 = arr1.includes(num) && arr2.includes(num);
+    if (!res1) {
+      return num;
+    }
+  };
+  newArr = mergeArr.filter(filterFunc);
+  return newArr.sort(compareFunc);
+}
+
+diffArray([1, 2, 3, 10, 5], [1, 2, 3, 4, 5]);
