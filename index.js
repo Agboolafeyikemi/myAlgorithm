272ab05843037bb9e19a8526bb1de2592e694099
agboola.omowumi <agboola.omowumi@konga.com>
2020-04-17 22:31:34 +0100

    Seek and destroy

--- index.js
+++ index.js
@@ -50,3 +50,27 @@ function diffArray(arr1, arr2) {
 }
 
 diffArray([1, 2, 3, 10, 5], [1, 2, 3, 4, 5]);
+
+//  Seek and Destroy
+// You will be provided with an initial array (the first argument in the destroyer function), followed by one or more arguments. Remove all elements from the initial array that are of the same value as these arguments.
+
+// Note
+// You have to use the arguments object.
+//SOLUTION
+function destroyer(...arr) {
+  let restArr = Array.from(arguments).slice(1);
+  let newArr = [];
+  arguments[0].map(el => {
+    if (!restArr.includes(el)) {
+      newArr.push(el);
+      return newArr;
+    }
+  });
+  return newArr;
+}
+
+destroyer([3, 5, 1, 2, 2], 2, 3, 5);
+
+//BEST SOLUTION
+const destroyer = (arr, ...valsToRemove) =>
+  arr.filter(elem => !valsToRemove.includes(elem));
