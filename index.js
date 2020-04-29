6fbf2915c4538f3c6fa0e66ecf8a210b860b4c66
agboola.omowumi <agboola.omowumi@konga.com>
2020-04-29 01:09:23 +0100

    Sum All Primes

--- index.js
+++ index.js
@@ -280,3 +280,32 @@ function sumFibs(num) {
 }
 
 sumFibs(10);
+
+
+//  Sum All Primes
+// A prime number is a whole number greater than 1 with exactly two divisors: 1 and itself. For example, 2 is a prime number because it is only divisible by 1 and 2. In contrast, 4 is not prime since it is divisible by 1, 2 and 4.
+
+// Rewrite sumPrimes so it returns the sum of all prime numbers that are less than or equal to num.
+// SOLUTION
+
+
+function sumPrimes(num) {
+
+  let numArr = [];
+  for(let i = 1; i <=num; i++) {
+      numArr.push(i)
+  }
+  return numArr.filter( isPrime => {
+       for(let j = 2; j < isPrime; j++){
+       if(isPrime % j == 0){return false}
+       }return isPrime !== 1 && isPrime !== 0;
+     }
+     ).reduce((a, b) => a+b)
+   
+  return numArr;
+}
+
+sumPrimes(10);
+
+
+
