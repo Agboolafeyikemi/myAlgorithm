427ba9617567eaa1dca617c4cadf2ecc4d681693
agboola.omowumi <agboola.omowumi@konga.com>
2020-05-01 17:32:26 +0100

    Drop it

--- index.js
+++ index.js
@@ -336,3 +336,35 @@ function smallestCommons(arr) {
     else return gcd(y, x % y);
   }
 }
+
+
+//  Drop it
+// Given the array arr, iterate through and remove each element starting from the first element (the 0 index) until the function func returns true when the iterated element is passed through it.
+
+// Then return the rest of the array once the condition is satisfied, otherwise, arr should be returned as an empty array.
+// SOLUTION
+
+function dropElements(arr, func) {
+  let newArr = []
+   let index = arr.findIndex(func)
+   if(index !== -1){
+    let res =  arr.slice(index)
+       newArr.push(...res)
+    } else  {
+       newArr = []
+    }
+  return newArr
+}
+
+dropElements([1, 2, 3, 4], function(n) {return n > 5;}) 
+
+//BEST SOLUTION
+
+function dropElements(arr, func) {
+  return arr.slice(arr.findIndex(func) >= 0 ? arr.findIndex(func) : arr.length);
+}
+
+// test here
+dropElements([1, 2, 3, 4], function(n) {
+  return n >= 3;
+});
