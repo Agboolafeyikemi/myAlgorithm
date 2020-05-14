43e273f7b153229af3f367f5989c788b7f2e50e5
agboola.omowumi <agboola.omowumi@konga.com>
2020-05-14 21:48:50 +0100

    Max_sales_ai1

--- index.js
+++ index.js
@@ -487,3 +487,23 @@ var Person = function(firstAndLast) {
 
 var bob = new Person("Bob Ross");
 bob.getFullName();
+
+
+// Calculate and print the sum of the elements in an array, keeping in mind that some of those integers may be quite large.
+
+// Function Description
+
+// Complete the aVeryBigSum function in the editor below. It must return the sum of all array elements.
+
+// aVeryBigSum has the following parameter(s):
+
+//Solution
+
+// Complete the aVeryBigSum function below.
+
+function aVeryBigSum(ar) {
+
+  return ar.reduce((num, cur) => {
+    return  num + cur
+  })
+ }
