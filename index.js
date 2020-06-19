e5c5575cac919ee731a7de06d7edd59c52ebe83d
agboola.omowumi <agboola.omowumi@konga.com>
2020-06-19 19:08:00 +0100

    maxSubarraySum

--- index.js
+++ index.js
@@ -1012,7 +1012,24 @@ singleNumber([4,1,2,1,2]);
 // Find the letter that was added in t.
 
 var findTheDifference = function(s, t) {
- return [...t].sort().find((char, i) =>  char !== [...s].sort()[i]) 
+ return [...t].sort().find((char, i) =>  char !== [...s].sort()[i]);
 };
 
 findTheDifference( "acdb", "abcde");
+
+function maxSubarraySum(arr, num){
+  let maxSum = 0;
+  let tempSum = 0;
+  if (arr.length < num) return null;
+  for (let i = 0; i < num; i++) {
+    maxSum += arr[i];
+  }
+  tempSum = maxSum;
+  for (let i = num; i < arr.length; i++) {
+    tempSum = tempSum - arr[i - num] + arr[i];
+    maxSum = Math.max(maxSum, tempSum);
+  }
+  return maxSum;
+}
+
+maxSubarraySum([2,6,9,2,1,8,5,6,3],3)
