5a5339757e3d9946446df3e9a82d6463b12c6904
Agboolafeyikemi <agboolafeyikemi93@gmail.com>
2021-01-13 21:09:01 +0100

    same parenthese

--- index.js
+++ index.js
@@ -1296,12 +1296,18 @@ var romanToInt = function(s) {
 
 var longestCommonPrefix = function(arr) {
   if (!arr.length) return "";
+  
   let prefix = "";
   for (let i = 0; i < arr[0].length; i++) {
+
       for (let j = 0; j < arr.length-1; j++) {
+
         console.log(arr.length-1, 'length')
+
         console.log(arr[j][i], 'arr[j][i]')
+
         if (arr[j][i] !== arr[j+1][i]) return prefix;
+
       }
       prefix += arr[0][i];
   }
