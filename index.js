6ea1f1dc06da6fbc8e55150903da9069066b74b5
Agboolafeyikemi <agboolafeyikemi93@gmail.com>
2021-01-13 21:08:28 +0100

    same parenthese

--- index.js
+++ index.js
@@ -1317,6 +1317,7 @@ longestCommonPrefix(["flower","flow","floight", "flo"]);
 // An input string is valid if:
 
 // Open brackets must be closed by the same type of brackets.
+
 // Open brackets must be closed in the correct order.
 
 
