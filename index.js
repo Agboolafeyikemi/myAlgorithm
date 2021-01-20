0ef6f1dc0adfe4fef03c271e3bab7c9067bfd863
Agboolafeyikemi <agboolafeyikemi93@gmail.com>
2021-01-20 11:37:08 +0100

    Repeated String

--- index.js
+++ index.js
@@ -1492,3 +1492,18 @@ var isValid = function(s) {
 
 
 
+// There is a string, , of lowercase English letters that is repeated infinitely many times. Given an integer, , find and print the number of letter a's in the first  letters of the infinite string.
+
+// Example
+
+
+// The substring we consider is , the first  characters of the infinite string. There are  occurrences of a in the substring.
+
+// Function Description
+
+// Complete the repeatedString function in the editor below.
+
+// repeatedString has the following parameter(s):
+
+// s: a string to repeat
+// n: the number of characters to conside
