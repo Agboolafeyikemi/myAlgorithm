005472cf5e1f1eb19983d0f62bdd954d2df4b566
Agboolafeyikemi <agboolafeyikemi93@gmail.com>
2021-01-23 23:56:38 +0100

    me

--- index.js
+++ index.js
@@ -1540,3 +1540,19 @@ function minimumDistances(a) {
 }
 
 minimumDistances([3,2,1,2,3]);
+
+var longestCommonPrefix = function(arr) {
+
+  if (!arr.length) return "";
+
+  let prefix = "";
+  for (let i = 0; i < arr[0].length; i++) {
+
+      for (let j = 0; j < arr.length-1; j++) {
+
+        if (arr[j][i] !== arr[j+1][i]) return prefix;
+
+      }
+      prefix += arr[0][i];
+  }
+  return prefix;
